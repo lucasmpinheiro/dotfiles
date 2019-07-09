7c3e99872b7b8b663e0068fb1a8315f23fe4a4cf
Lucas Pinheiro <lucasmpinheiro@gmail.com>
2019-07-09 12:31:18 -0300

    awesomewm: Change default editor to vim.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -62,7 +62,7 @@ beautiful.init("/home/lucasmp/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
-editor = os.getenv("EDITOR") or "nano"
+editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
