185e22b875e7579b8d559a14d8681d7102de3bac
Lucas Pinheiro <lucasmpinheiro@gmail.com>
2019-07-25 16:05:07 -0300

    polybar: Add external monitor top bar to launch script.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,5 +9,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar top &
 
+my_laptop_external_monitor=$(xrandr --query | grep 'HDMI-1')
+if [[ $my_laptop_external_monitor = *connected* ]]; then
+    polybar top_external &
+fi
+
 echo "Polybar launched..."
 
