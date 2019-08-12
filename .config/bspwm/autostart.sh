9230273be6ac94e8aa3f546c214c06d4bc3a717a
Lucas Pinheiro <lucasmpinheiro@gmail.com>
2019-08-12 19:34:48 -0300

    bspwm: Create applications autostart script.

---
+++ .config/bspwm/autostart.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Autostart applications.
+
+sxhkd &
+
+$HOME/.config/polybar/launch.sh
+
+eval $(ssh-agent) &
+lxsession &
+compton &
+setxkbmap -layout br,us &
+light-locker --lock-on-suspend &
+nitrogen --restore &
+# redshift &
+run nm-applet &
+run pamac-tray &
+run xfce4-clipman &
+run com.slack.Slack &
+run thunderbird &
+
