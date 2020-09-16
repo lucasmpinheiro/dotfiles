53855a3e9a9f7088818853b8506626150d22e616
Lucas Pinheiro <lucasmpinheiro@gmail.com>
2020-09-16 17:24:25 -0300

    fish: Use starship instead of powerline and update env.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,22 +9,32 @@ set EDITOR $VISUAL
 eval (keychain --eval --quiet)
 
 # Setup JAVA home.
-set JAVA_HOME $HOME/.sdkman/candidates/java/current
+#set JAVA_HOME $HOME/.sdkman/candidates/java/current
 
 # Setup powerline.
-set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish"
-powerline-setup
+#set fish_function_path $fish_function_path "$HOME/.local/lib/python3.8/site-packages/powerline/bindings/fish"
+#powerline-setup
 
 # Activate default RVM ruby.
-set PATH $PATH:$HOME/.rvm/bin
-rvm default
+#set PATH $PATH:$HOME/.rvm/bin
+#rvm default
 
 # Setup NVM to work on fish.
 nvm --version > /dev/null
+
+# Add rust, cargo and rustup to the path.
+set PATH $PATH:$HOME/.cargo/bin
+
+set QT_QPA_PLATFORMTHEME qt5ct
 
 # Setup SDKMan.
 #source "$HOME/.sdkman/bin/sdkman-init.sh"
 
 # Run neofetch.
-neofetch
+# neofetch
+
+# Add flutter to the path.
+# set PATH $PATH:$HOME/workspace/flutter/bin
+
+starship init fish | source
 
