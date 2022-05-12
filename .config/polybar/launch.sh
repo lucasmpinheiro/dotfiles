bfd28625eabd0087e97b1b8c0957239b7879fe1a
Lucas Pinheiro <lucasmpinheiro@gmail.com>
2022-05-12 09:41:08 -0300

    Update polybar config.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,12 +7,19 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar top &
+# polybar top &
 
-my_laptop_external_monitor=$(xrandr --query | grep 'HDMI-1')
-if [[ $my_laptop_external_monitor = *connected* ]]; then
-    polybar top_external &
-fi
+# my_laptop_external_monitor=$(xrandr --query | grep 'HDMI-1')
+# if [[ $my_laptop_external_monitor = *connected* ]]; then
+#     polybar top_external &
+# fi
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar mainbar-herbstluftwm &
+
+# my_laptop_external_monitor=$(xrandr --query | grep 'HDMI-1')
+# if [[ $my_laptop_external_monitor = *connected* ]]; then
+#     polybar mainbar-herbstluftwm-extra &
+# fi
 
 echo "Polybar launched..."
-
