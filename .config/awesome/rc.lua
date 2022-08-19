14535721443d2d99d64d4741d06ee4005beeb204
Lucas Moraes Pinheiro <lucasmpinheiro@gmail.com>
2022-08-19 19:28:04 -0300

    [awesomewm] Move client keys config to a separate file.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -22,9 +22,6 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
 
--- Other libraries.
-local cyclefocus = require('cyclefocus')
-
 -- Load theme.
 beautiful.init(require("theme"))
 local theme = beautiful.get()
@@ -55,6 +52,7 @@ awful.layout.layouts = require('configuration.layouts')
 
 -- Load other configs.
 local tags = require("configuration.tags")
+local client_keys = require("configuration.client.keys")
 
 awful.screen.connect_for_each_screen(
     function(s)
@@ -96,7 +94,6 @@ awful.screen.connect_for_each_screen(
             {
                 -- Left widgets
                 layout = wibox.layout.fixed.horizontal,
-                mylauncher,
                 s.mytaglist,
                 s.mypromptbox
             },
@@ -127,104 +124,6 @@ root.buttons(
 
 -- {{{ Key bindings
 local globalkeys = require("configuration.keys.global")
-
-clientkeys =
-    gears.table.join(
-    awful.key(
-        {modkey},
-        "f",
-        function(c)
-            c.fullscreen = not c.fullscreen
-            c:raise()
-        end,
-        {description = "toggle fullscreen", group = "client"}
-    ),
-    awful.key(
-        {modkey},
-        "w",
-        function(c)
-            c:kill()
-        end,
-        {description = "close", group = "client"}
-    ),
-    awful.key(
-        {modkey, "Control"},
-        "space",
-        awful.client.floating.toggle,
-        {description = "toggle floating", group = "client"}
-    ),
-    awful.key(
-        {modkey, "Control"},
-        "Return",
-        function(c)
-            c:swap(awful.client.getmaster())
-        end,
-        {description = "move to master", group = "client"}
-    ),
-    awful.key(
-        {modkey},
-        "o",
-        function(c)
-            c:move_to_screen()
-        end,
-        {description = "move to screen", group = "client"}
-    ),
-    awful.key(
-        {modkey},
-        "t",
-        function(c)
-            c.ontop = not c.ontop
-        end,
-        {description = "toggle keep on top", group = "client"}
-    ),
-    awful.key(
-        {modkey},
-        "n",
-        function(c)
-            -- The client currently has the input focus, so it cannot be
-            -- minimized, since minimized clients can't have the focus.
-            c.minimized = true
-        end,
-        {description = "minimize", group = "client"}
-    ),
-    awful.key(
-        {modkey},
-        "m",
-        function(c)
-            c.maximized = not c.maximized
-            c:raise()
-        end,
-        {description = "(un)maximize", group = "client"}
-    ),
-    awful.key(
-        {modkey, "Control"},
-        "m",
-        function(c)
-            c.maximized_vertical = not c.maximized_vertical
-            c:raise()
-        end,
-        {description = "(un)maximize vertically", group = "client"}
-    ),
-    awful.key(
-        {modkey, "Shift"},
-        "m",
-        function(c)
-            c.maximized_horizontal = not c.maximized_horizontal
-            c:raise()
-        end,
-        {description = "(un)maximize horizontally", group = "client"}
-    ),
-    -- Alt-Tab: cycle through clients on the same screen.
-    -- This must be a clientkeys mapping to have source_c available in the callback.
-    cyclefocus.key({altkey}, "Tab", {
-        -- cycle_filters as a function callback:
-        -- cycle_filters = { function (c, source_c) return c.screen == source_c.screen end },
-
-        -- cycle_filters from the default filters:
-        cycle_filters = { cyclefocus.filters.same_screen, cyclefocus.filters.common_tag },
-        keys = {'Tab', 'ISO_Left_Tab'}  -- default, could be left out
-    })
-)
 
 clientbuttons =
     gears.table.join(
@@ -268,7 +167,7 @@ awful.rules.rules = {
             border_color = beautiful.border_normal,
             focus = awful.client.focus.filter,
             raise = true,
-            keys = clientkeys,
+            keys = client_keys,
             buttons = clientbuttons,
             screen = awful.screen.preferred,
             placement = awful.placement.no_overlap + awful.placement.no_offscreen
