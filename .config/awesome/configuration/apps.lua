916110f310f69d5a99e3cec358f1c9796a537f26
Lucas Moraes Pinheiro <lucasmpinheiro@gmail.com>
2022-08-24 11:03:37 -0300

    [awesomewm] Change default editor and browser apps.

--- .config/awesome/configuration/apps.lua
+++ .config/awesome/configuration/apps.lua
@@ -1,14 +1,15 @@
 local os = os
 
-local filesystem = require('gears.filesystem')
+local filesystem = require("gears.filesystem")
 
 return {
-  -- List of apps to start by default on some actions.
-  default = {
-    terminal = "kitty",
-    editor = os.getenv("EDITOR") or "vim",
-    run_app = "rofi -modi drun,window,run,ssh -show drun",
-    lock = 'slock',
-    quake = "kitty --title QuakeTerminal"
-  }
+	-- List of apps to start by default on some actions.
+	default = {
+		terminal = "kitty",
+		editor = os.getenv("EDITOR") or "code",
+		run_app = "rofi -modi drun,window,run,ssh -show drun",
+		lock = "slock",
+		browser = "firefox",
+		quake = "kitty --title QuakeTerminal",
+	},
 }
