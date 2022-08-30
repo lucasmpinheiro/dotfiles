1e6c9b3ade66536d6391341a0576f0717047ac21
Lucas Moraes Pinheiro <lucasmpinheiro@gmail.com>
2022-08-30 17:14:18 -0300

    [awesomewm] Use generic list widget for volume.

--- .config/awesome/widget/volume/init.lua
+++ .config/awesome/widget/volume/init.lua
@@ -1,44 +1,50 @@
 local lain = require("lain")
+local wibox = require("wibox")
 local awful = require("awful")
+
+local icons = require("theme.icons")
+local list_item_widget = require("widget.generic.list-item")
 
 local os = os
 
--- Volume widget.
-local volume_widget = lain.widget.pulse({
-	settings = function()
-		local volicon = " "
-		local vlevel = tonumber(volume_now.left)
+local not_available_text = "NA"
 
+-- Volume widget.
+local wrapper = lain.widget.pulse({
+	settings = function()
+		local vlevel = tonumber(volume_now.left) or not_available_text
+
+		local volicon = icons.volume_high
 		if volume_now.muted == "yes" or vlevel == nil then
-			volicon = "ﱝ "
+			volicon = icons.volume_muted
 		elseif vlevel == 0 then
-			volicon = " "
+			volicon = icons.volume_off
 		elseif vlevel < 50 then
-			volicon = " "
+			volicon = icons.volume_low
 		end
 
-		vlevel = vlevel or "NA"
-
-		widget:set_markup(" " .. volicon .. vlevel .. "% ")
+		widget:set_icon(volicon)
+		widget:set_text(("%3d"):format(vlevel) .. "%")
 	end,
+	widget = list_item_widget(icons.volume_off, not_available_text),
 })
 
-volume_widget.widget:buttons(awful.util.table.join(
+wrapper.widget:buttons(awful.util.table.join(
 	awful.button({}, 1, function() -- left click
 		awful.spawn("pavucontrol")
 	end),
 	awful.button({}, 3, function() -- right click
-		os.execute(string.format("pactl set-sink-volume %s toggle", volume_widget.device))
-		volume_widget.update()
+		os.execute(string.format("pactl set-sink-mute %s toggle", wrapper.device))
+		wrapper.update()
 	end),
 	awful.button({}, 4, function() -- scroll up
-		os.execute(string.format("pactl set-sink-volume %s +5%%", volume_widget.device))
-		volume_widget.update()
+		os.execute(string.format("pactl set-sink-volume %s +5%%", wrapper.device))
+		wrapper.update()
 	end),
 	awful.button({}, 5, function() -- scroll down
-		os.execute(string.format("pactl set-sink-volume %s -5%%", volume_widget.device))
-		volume_widget.update()
+		os.execute(string.format("pactl set-sink-volume %s -5%%", wrapper.device))
+		wrapper.update()
 	end)
 ))
 
-return volume_widget
+return wrapper.widget
