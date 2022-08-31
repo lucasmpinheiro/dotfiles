67652f93aa68f877fc48b672cf009f76597d1966
Lucas Moraes Pinheiro <lucasmpinheiro@gmail.com>
2022-08-31 10:14:10 -0300

    [awesomewm] Fix: Add icons list module.

---
+++ .config/awesome/theme/icons/init.lua
@@ -0,0 +1,8 @@
+local dir = os.getenv("HOME") .. "/.config/awesome/theme/icons"
+
+return {
+	volume_high = dir .. "/volume/volume-high-solid.svg",
+	volume_low = dir .. "/volume/volume-low-solid.svg",
+	volume_off = dir .. "/volume/volume-off-solid.svg",
+	volume_muted = dir .. "/volume/volume-xmark-solid.svg",
+}
