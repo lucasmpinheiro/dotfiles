9ce848ee6d2f53a9918330b6216adbd82567ca1f
Lucas Moraes Pinheiro <lucasmpinheiro@gmail.com>
2022-09-12 20:43:34 -0300

    [awesomewm] Use generic list item widget for battery widget.

--- .config/awesome/widget/battery/init.lua
+++ .config/awesome/widget/battery/init.lua
@@ -1,31 +1,36 @@
 local lain = require("lain")
 
+local icons = require("theme.icons")
+local list_item_widget = require("widget.generic.list-item")
+
 -- Battery widget.
-local battery_widget = lain.widget.bat {
-  timeout = 5,
-  settings = function()
-      if bat_now.status == nil or bat_now.status == "N/A" then
-          widget:set_markup()
-          return
-      end
+local wrapper = lain.widget.bat({
+	timeout = 5,
+	settings = function()
+		if bat_now.status == nil or bat_now.status == "N/A" then
+			widget:set_content(nil, "")
+			return
+		end
 
-      local baticon = "  "
-      local blevel = tonumber(bat_now.perc)
+		local blevel = tonumber(bat_now.perc)
 
-      if bat_now.ac_status == 1 then
-          baticon = " "
-      elseif blevel <= 5 then
-          baticon = "  "
-      elseif blevel <= 25 then
-          baticon = "  "
-      elseif blevel <=50 then
-          baticon = "  "
-      elseif blevel <= 75 then
-          baticon = "  "
-      end
+		local baticon = icons.battery_full
+		if bat_now.ac_status == 1 then
+			baticon = icons.battery_charging
+		elseif blevel <= 5 then
+			baticon = icons.battery_empty
+		elseif blevel <= 25 then
+			baticon = icons.battery_low
+		elseif blevel <= 50 then
+			baticon = icons.battery_medium
+		elseif blevel <= 75 then
+			baticon = icons.battery_high
+		end
 
-      widget:set_markup(" " .. baticon .. bat_now.perc .. "% ")
-  end
-}
+		-- widget:set_markup(" " .. baticon .. bat_now.perc .. "% ")
+		widget:set_content(baticon, ("%-4s"):format(bat_now.perc .. "%"))
+	end,
+	widget = list_item_widget(nil, ""),
+})
 
-return battery_widget
+return wrapper.widget
