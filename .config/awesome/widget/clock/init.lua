b1052aaf6d006506c10af69f774f6bbda0c8ed10
Lucas Moraes Pinheiro <lucasmpinheiro@gmail.com>
2022-09-13 16:43:13 -0300

    [awesomewm] Change clock widget to work with vertical wibar.

--- .config/awesome/widget/clock/init.lua
+++ .config/awesome/widget/clock/init.lua
@@ -2,18 +2,25 @@ local wibox = require("wibox")
 local lain = require("lain")
 local theme = require("theme")
 
-local clock_widget = wibox.widget.textclock()
+-- local clock_widget = wibox.widget.textclock("%H:%M")
+
+local clock_widget = wibox.widget(
+    {
+      {format = "%H:%M", widget = wibox.widget.textclock},
+      layout = wibox.layout.flex.horizontal,
+    }
+)
 
 -- Attach a calendar notification to the clock widget.
 lain.widget.cal {
-    attach_to = { clock_widget },
-    week_start = 1,
-    followtag = true,
-    notification_preset = {
-        font = theme.font,
-        fg = theme.fg_normal,
-        bg = theme.bg_normal,
-    },
+  attach_to = {clock_widget},
+  week_start = 1,
+  followtag = true,
+  notification_preset = {
+    font = theme.font,
+    fg = theme.fg_normal,
+    bg = theme.bg_normal,
+  },
 }
 
 return clock_widget
