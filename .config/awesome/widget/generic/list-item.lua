86a04487b82c9946deaf6630eb72b371c0f02ac9
Lucas Moraes Pinheiro <lucasmpinheiro@gmail.com>
2022-09-25 16:58:27 -0300

    [awesomewm] Format list item widget module.

--- .config/awesome/widget/generic/list-item.lua
+++ .config/awesome/widget/generic/list-item.lua
@@ -1,54 +1,54 @@
 local wibox = require("wibox")
-local theme = require('beautiful')
+local theme = require("beautiful")
 
 local factory = function(icon, text, bg_color)
-	bg_color = bg_color or theme.bg_normal
+  bg_color = bg_color or theme.bg_normal
 
-	return wibox.widget({
-		{
-			{
-				{
-					{
-						{
-							id = "icon",
-							image = icon,
-							resize = true,
-							widget = wibox.widget.imagebox,
-						},
-						height = 14,
-						widget = wibox.container.constraint,
-					},
-					valign = "center",
-					widget = wibox.container.place,
-				},
-				{
-					{
-						id = "txt",
-						text = text,
-						widget = wibox.widget.textbox,
-					},
-					left = 4,
-					widget = wibox.container.margin,
-				},
-				layout = wibox.layout.align.horizontal,
-			},
-			left = 4,
-			right = 4,
-			layout = wibox.container.margin,
-		},
-		bg = bg_color,
-		widget = wibox.container.background,
-		set_icon = function(self, icon)
-			self:get_children_by_id("icon")[1]:set_image(icon)
-		end,
-		set_text = function(self, text)
-			self:get_children_by_id("txt")[1]:set_text(text)
-		end,
-		set_content = function(self, icon, text)
-			self:set_icon(icon)
-			self:set_text(text)
-		end,
-	})
+  return wibox.widget {
+    {
+      {
+        {
+          {
+            {
+              id = "icon",
+              image = icon,
+              resize = true,
+              widget = wibox.widget.imagebox,
+            },
+            height = 14,
+            widget = wibox.container.constraint,
+          },
+          valign = "center",
+          widget = wibox.container.place,
+        },
+        {
+          {
+            id = "txt",
+            text = text,
+            widget = wibox.widget.textbox,
+          },
+          left = 4,
+          widget = wibox.container.margin,
+        },
+        layout = wibox.layout.align.horizontal,
+      },
+      left = 4,
+      right = 4,
+      layout = wibox.container.margin,
+    },
+    bg = bg_color,
+    widget = wibox.container.background,
+    set_icon = function(self, icon)
+      self:get_children_by_id("icon")[1]:set_image(icon)
+    end,
+    set_text = function(self, text)
+      self:get_children_by_id("txt")[1]:set_text(text)
+    end,
+    set_content = function(self, icon, text)
+      self:set_icon(icon)
+      self:set_text(text)
+    end,
+  }
 end
 
 return factory
