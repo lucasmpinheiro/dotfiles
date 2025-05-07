ee32ca8289cb215de49a9d72d28f9c8d1bb323a2
Lucas Pinheiro <lucasmpinheiro@gmail.com>
2025-05-07 18:25:07 -0300

    chore: update sketchybar config

---
+++ .config/sketchybar/items/calendar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sketchybar --add item calendar right \
+           --set calendar icon=􀧞  \
+                          update_freq=10 \
+                          script="$PLUGIN_DIR/clock.sh"
+
---
+++ .config/sketchybar/items/spaces.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sketchybar --add event aerospace_workspace_change
+
+for sid in $(aerospace list-workspaces --all); do
+    sketchybar  --add item space.$sid left \
+                --subscribe space.$sid aerospace_workspace_change \
+                --set space.$sid \
+                      background.drawing=off \
+	                    label.padding_left=3 \
+	                    label.padding_right=3 \
+                      label.font="sketchybar-app-font:Regular:16.0" \
+	                    icon.padding_left=0 \
+	                    icon.padding_right=0 \
+                      icon="$sid" \
+                      click_script="aerospace workspace $sid" \
+                      script="$CONFIG_DIR/plugins/aerospace.sh $sid"
+done
+
+sketchybar --add item space_separator left \
+           --set space_separator icon="􀆊" \
+                                 icon.color=$ACCENT_COLOR \
+                                 icon.padding_left=4 \
+                                 label.drawing=off \
+                                 background.drawing=off
+#                                 script="$PLUGIN_DIR/space_windows.sh" \
+#           --subscribe space_separator space_windows_change
+
