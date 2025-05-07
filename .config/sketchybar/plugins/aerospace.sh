c568d9e1fb0d403e54cef032fa8a73ef8371337d
Lucas Pinheiro <lucasmpinheiro@gmail.com>
2025-05-07 18:23:59 -0300

    chore: update sketchybar workspaces colors

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+source "$CONFIG_DIR/colors.sh"
+
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
+    sketchybar --set $NAME background.drawing=on \
+                           label.color=$BAR_COLOR \
+                           icon.color=$BAR_COLOR
 else
-    sketchybar --set $NAME background.drawing=off
+    sketchybar --set $NAME background.drawing=off \
+                           label.color=$ACCENT_COLOR \
+                           icon.color=$ACCENT_COLOR
 fi
 
