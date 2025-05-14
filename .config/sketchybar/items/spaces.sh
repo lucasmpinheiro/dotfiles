d415ea9f4a340d83787a9c565aadf213c4459e5c
Lucas Pinheiro <lucasmpinheiro@gmail.com>
2025-05-14 17:48:24 -0300

    fix: sketchybarr spacing and left items order

--- .config/sketchybar/items/spaces.sh
+++ .config/sketchybar/items/spaces.sh
@@ -7,11 +7,14 @@ for sid in $(aerospace list-workspaces --all); do
                 --subscribe space.$sid aerospace_workspace_change \
                 --set space.$sid \
                       background.drawing=off \
-	                    label.padding_left=3 \
-	                    label.padding_right=3 \
+                      background.color=$ACCENT_COLOR \
+                      padding_left=0 \
+                      padding_right=0 \
+	                    label.padding_left=0 \
+	                    label.padding_right=0 \
                       label.font="sketchybar-app-font:Regular:16.0" \
-	                    icon.padding_left=0 \
-	                    icon.padding_right=0 \
+	                    icon.padding_left=6 \
+	                    icon.padding_right=6 \
                       icon="$sid" \
                       click_script="aerospace workspace $sid" \
                       script="$CONFIG_DIR/plugins/aerospace.sh $sid"
