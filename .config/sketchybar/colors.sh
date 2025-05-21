e5e42a4ea8020dc2b39661769ec8c5efe8ceaef4
Lucas Pinheiro <lucasmpinheiro@gmail.com>
2025-05-21 15:13:22 -0300

    chore: change sketchybar theme

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
 
 export WHITE=0xffffffff
+export MAUVE=0xffc6a0f6
+export RED=0xffed8796
+export PEACH=0xfff5a97f
+export YELLOW=0xffeed49f
+export GREEN=0xffa6da95
+export TEAL=0xff8bd5ca
+export BLUE=0xff8aadf4
+export LAVENDER=0xffb7bdf8
 
 # THEME COLORS
 # These should not be used directly. They are to be referenced by the bar colors instead.
 export BASE_COLOR=0xff24273a
 export OVERLAY_COLOR=0xff6e738d
+export SURFACE_COLOR=0xff494d64
 export TEXT_COLOR=0xffcdd6f4
 export ACCENT_COLOR=0xff8aadf4
 
-# BAR COLORS 
+# BAR COLORS
 # Variables actually used by bar components.
-export BAR_COLOR=0x00000000
-export PRIMARY_ITEM_BG_COLOR=$ACCENT_COLOR
-export PRIMARY_ITEM_LABEL_COLOR=$BASE_COLOR
+export BAR_COLOR=0xaa24273a
+export PRIMARY_ITEM_BG_COLOR=$SURFACE_COLOR
+export PRIMARY_ITEM_LABEL_COLOR=$TEXT_COLOR
 export PRIMARY_ITEM_ICON_COLOR=$BASE_COLOR
 export SECONDARY_ITEM_BG_COLOR=$OVERLAY_COLOR
 export SECONDARY_ITEM_LABEL_COLOR=$BASE_COLOR
 export SECONDARY_ITEM_ICON_COLOR=$TEXT_COLOR
-
--- .config/sketchybar/items/front_app.sh
+++ .config/sketchybar/items/front_app.sh
@@ -4,15 +4,18 @@ sketchybar --add item space_separator left \
            --set space_separator icon="􀆊" \
                                  icon.padding_left=4 \
                                  icon.padding_right=4 \
+                                 icon.color=$TEXT_COLOR \
                                  label.padding_right=0 \
                                  padding_right=0 \
-                                 label.drawing=off
+                                 label.drawing=off \
+                                 background.drawing=off
 
 sketchybar  --add item front_app left \
             --set front_app \
                   icon.font="sketchybar-app-font:Regular:16.0" \
+                  icon.color=$TEXT_COLOR \
                   label.padding_left=0 \
-                  icon.padding_left=2 \
                   padding_left=0 \
+                  background.drawing=off \
                   script="$PLUGIN_DIR/front_app.sh" \
             --subscribe front_app front_app_switched
