1674b8369c2ac9fe9d676edbaaefd110f34ef01d
Lucas Pinheiro <lucasmpinheiro@gmail.com>
2025-05-21 14:02:07 -0300

    feat: add app icons on MacOS workspaces

--- .config/sketchybar/items/spaces.sh
+++ .config/sketchybar/items/spaces.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
-sketchybar --add event aerospace_workspace_change
+sketchybar --add event aerospace_workspace_change \
+           --add event aerospace_window_focus_change
 
 for sid in $(aerospace list-workspaces --all); do
+    # Add the workspace number item
     sketchybar  --add item space.$sid left \
                 --subscribe space.$sid aerospace_workspace_change \
                 --set space.$sid \
-                      background.color=$SECONDARY_ITEM_BG_COLOR \
+                      background.corner_radius=0 \
                       padding_left=0 \
                       padding_right=0 \
-	                    label.padding_left=0 \
-	                    label.padding_right=0 \
+                      label.padding_left=0 \
+                      label.padding_right=0 \
                       label.font="sketchybar-app-font:Regular:16.0" \
-	                    icon.padding_left=6 \
-	                    icon.padding_right=6 \
+                      icon.padding_left=6 \
+                      icon.padding_right=6 \
                       icon="$sid" \
                       click_script="aerospace workspace $sid" \
                       script="$CONFIG_DIR/plugins/aerospace.sh $sid"
+
+    # Add the workspace apps item
+    sketchybar  --add item space.$sid.apps left \
+                --subscribe space.$sid.apps aerospace_window_focus_change \
+                --set space.$sid.apps \
+                      background.corner_radius=0 \
+                      padding_left=0 \
+                      padding_right=4 \
+                      label.padding_left=0 \
+                      label.padding_right=4 \
+                      label.font="sketchybar-app-font:Regular:12.0" \
+                      icon.drawing=off \
+                      script="$CONFIG_DIR/plugins/workspace_apps.sh $sid"
 done
 
--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -3,12 +3,10 @@
 source "$CONFIG_DIR/colors.sh"
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.color=$PRIMARY_ITEM_BG_COLOR \
-                           icon.color=$PRIMARY_ITEM_ICON_COLOR \
+    sketchybar --set $NAME icon.color=$RED \
                            label.color=$PRIMARY_ITEM_LABEL_COLOR
 else
-    sketchybar --set $NAME background.color=$SECONDARY_ITEM_BG_COLOR \
-                           icon.color=$SECONDARY_ITEM_ICON_COLOR \
+    sketchybar --set $NAME icon.color=$SECONDARY_ITEM_ICON_COLOR \
                            label.color=$SECONDARY_ITEM_LABEL_COLOR
 fi
 
